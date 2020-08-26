019158bb92e66b402c60a9e9321b283c1c1cde3c
aelgmal7 <aelgmal7@gmail.com>
2020-08-27 00:00:26 +0200

    model added

---
+++ src/app/shared/models/orderModel.ts
@@ -0,0 +1,26 @@
+  // tslint:disable-next-line: semicolon
+
+export class OrderModel{
+  public facebookAdminName: string;
+  public orderDate: Date;
+  public deliveryName:string;
+  public itemName:string;
+  public itemCost:number;
+  public delivryCost:number;
+  public clientName:string;
+  public clientPhoneNumber:number;
+  // tslint:disable-next-line: align
+ public address:{
+   region:string,
+   addressInDetails:string
+
+ }
+ public orderState:{
+   onDelivry:boolean,
+   delvired:boolean,
+  rejected:boolean
+  }
+
+
+
+}
