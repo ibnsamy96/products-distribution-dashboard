83c7e71f4d2cc26d60ece00a5c967d6a29f8fb7f
mahmoud-ibn-samy <mahmoudsamy50@outlook.com>
2020-08-27 00:03:57 +0200

    modifing ordermodel

--- src/app/shared/models/orderModel.ts
+++ src/app/shared/models/orderModel.ts
@@ -1,25 +1,26 @@
-  // tslint:disable-next-line: semicolon
+// tslint:disable-next-line: semicolon
 
-export class OrderModel{
+export class OrderModel {
+
   public facebookAdminName: string;
   public orderDate: Date;
-  public deliveryName:string;
-  public itemName:string;
-  public itemCost:number;
-  public delivryCost:number;
-  public clientName:string;
-  public clientPhoneNumber:number;
+  public deliveryName: string;
+  public itemName: string;
+  public itemCost: number;
+  public deliveryCost: number;
+  public clientName: string;
+  public clientPhoneNumber: number;
   // tslint:disable-next-line: align
- public address:{
-   region:string,
-   addressInDetails:string
+  public address: {
+    region: string,
+    addressInDetails: string
 
- }
- public orderState:{
-   onDelivry:boolean,
-   delvired:boolean,
-  rejected:boolean
-  }
+  };
+  public orderState: {
+    onDelivery: boolean,
+    delivered: boolean,
+    rejected: boolean
+  };
 
 
 
