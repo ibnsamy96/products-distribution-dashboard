e527be070c2a84eac491e0353c243f73361f1336
aelgmal7 <aelgmal7@gmail.com>
2020-08-28 20:35:20 +0200

    stock added

--- src/app/shared/models/ItemModel.ts
+++ src/app/shared/models/ItemModel.ts
@@ -1,14 +1,8 @@
 export class ItemModel {
-<<<<<<< HEAD
   public itemName: string ;
   public itemCost ?: number ;
   public itemAmount ?: number ;
   public itemState ?: boolean ;
 
-=======
-  public itemName: string;
-  public itemCost: number;
-  public itemNumber: number;
-  public itemState: boolean;
->>>>>>> 9917fc85703cf13855948a139805f8a2d70aa9dc
+
 }
