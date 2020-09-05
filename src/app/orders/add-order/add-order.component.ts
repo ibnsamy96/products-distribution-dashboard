541c090263a0794f07c7277373e4a25492aae635
mahmoud-ibn-samy <mahmoudsamy50@outlook.com>
2020-09-05 07:08:55 +0200

    translated add-order form and implemented its logic

--- src/app/orders/add-order/add-order.component.ts
+++ src/app/orders/add-order/add-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-order',
@@ -9,7 +10,17 @@ export class AddOrderComponent implements OnInit {
 
   constructor() { }
 
+  deliveryMen: { name: string, id: string }[] = [
+    { name: 'محمود', id: 'dsdsdw2edd' },
+    { name: 'محمد', id: 'dsdsdw2edd' },
+    { name: 'أحمد', id: 'dsdsdw2edd' },
+  ];
+
   ngOnInit(): void {
   }
 
+  submitNewOrder(newOrderForm: NgForm): void {
+    console.log(newOrderForm.value);
+
+  }
 }
